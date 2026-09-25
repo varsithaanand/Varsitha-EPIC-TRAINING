3ffae9f1f834dfad97bbc82ec325df113510d583
Varsitha Anand Kumar <varshithaanand2007@gmail.com>
2026-09-25 20:42:17 +0530

    Update LinkedList2.java

--- java_Concepts/src/dataStructures/LinkedList2.java
+++ java_Concepts/src/dataStructures/LinkedList2.java
@@ -1,92 +1,163 @@
-package dataStructures;
 import java.util.Scanner;
-class Node1
+class Customer
 {
-	int data;
-	Node1 address;
-	Node1(int data,Node1 address)
-	{
-		this.data=data;
-		this.address=address;
-	}
-	Node1()
-	{
-		
-	}
-	Node1 head=null,prev=null;
-	void insert(Scanner in)
-	{
-		System.out.println("Enter the no of values to enter");
-		int n=in.nextInt();
-		
-		for(int i=0;i<n;i++)
-		{
-			//System.out.println("Enter the no");
-			int data=in.nextInt();
-			Node1 obj=new Node1(data,null);
-			if(head==null)
-			{
-				head=obj;
-				prev=obj;
-				
-			}
-			else
-			{
-				prev.address=obj;
-				prev=obj;
-			}
-			
-		}
-	}
-	
-	void display()
-	{
-		Node1 temp=head;
-		while(temp!=null)
-		{
-		    System.out.println(temp.data);
-		    System.out.println(temp.address);
-		    temp=temp.address;
-		}
-	}
-	void insertANode(Scanner in){
-        System.out.println("Enter the value: ");
-        int val = in.nextInt();//55
-        System.out.println("Enter the position: ");
-        int pos = in.nextInt();//4
+    int id=0;
+    String cusName;
+    String cusEmail;
+    Node li;
+    Customer(Node li)
+    {
+       this.li=li; 
+    }
+    Customer(int id,String name,String email)
+    {
+        this.id=id;
+        this.cusName=name;
+        this.cusEmail=email;
+    }
+    Scanner in=new Scanner(System.in);
+    void createCustomer()
+    {
         
-        Node1 newNode = new Node1(val,null);//8000
-        if(pos==1){
-            newNode.address = head;
-            head = newNode;
-        }
-        else{
-        Node1 temp = head;//1000
+        System.out.println("Enter the Customer Name ");
+        String name=in.next();
+        System.out.println("Enter the Customer Email ");
+        String email=in.next();
+         id++;
+        Customer cus=new Customer(id,name,email);
+        li.insertNode(in,cus);
         
-        for(int i=0;i<pos-2;i++){
-            temp=temp.address;
-        }
-        if(temp.address==null)
-        {
-        	prev=newNode;
-        }
-        newNode.address = temp.address;
-        temp.address = newNode;
-        }
         
     }
-		
-	}
-
-public class LinkedList2 {
-
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		Node1 n=new Node1();
-		n.insert(in);
-		n.display();
-        n.insertANode(in);
-        n.display();
-	}
-
+    void displayCustomer()
+    {
+        li.display();
+    }
+    void deleteCustomer()
+    {
+        li.deleteNote(in);
+    }
 }
+class Node {
+    Customer data;
+    Node next;
+    Node(Customer data,Node next)
+    {
+        this.data=data;
+        this.next=next;
+    }
+    Node()
+    {
+        
+    }
+    Node head=null,tail=null;
+    void insertNode(Scanner in,Customer c)
+    {
+        
+        Node obj=new Node(c,null);
+        if(head==null)
+        {
+            head=obj;
+            tail=obj;
+        }
+        else{
+            tail.next=obj;
+            tail=obj;
+        }
+         System.out.println("inserted"); 
+        }
+       
+    
+    void display()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.println("Customer Id "+temp.data.id);
+            System.out.println("Customer Name "+temp.data.cusName);
+            System.out.println("Customer Email "+temp.data.cusEmail);
+            temp=temp.next;
+        }
+    }
+    
+    
+    void deleteNote(Scanner in)
+    {
+        System.out.println("enter the pos"); 
+        int pos=in.nextInt();
+        Node temp=head;
+        Node prev=null;
+        if(pos==1)
+        {
+            head=temp.next;
+        }
+        else
+        {
+          for(int i=0;i<pos-1;i++)
+          {
+            prev=temp;
+            temp=temp.next;
+          }
+          prev.next=temp.next;
+          if(prev.next==null)
+          {
+              tail=prev;
+          }
+          
+        }
+       System.out.println("deleted"); 
+    }
+    
+}
+    public class Main
+    {
+        public static void main (String[] args) 
+        {
+            Scanner s=new Scanner(System.in);
+            Node in=new Node();
+            Customer cus=new Customer(in);
+            while(true)
+            {
+                
+                System.out.println("1.Create Customer");
+                System.out.println("2.Display Customer");
+                System.out.println("3.Delete Customer");
+                System.out.println("4.display");
+                System.out.println("exit");
+                int ch=s.nextInt();
+                switch(ch)
+                {
+                  case 1:
+                {
+                    cus.createCustomer();
+                    break;
+                }  
+                  case 2 :
+                {
+                    cus.displayCustomer();
+                    break;
+                }
+                case 3 :
+                {
+                    cus.deleteCustomer();
+                    break;
+                }
+                case 4 :
+                {
+                      in.display();
+                      break;
+                }
+                
+                default :
+                {
+                      return;
+                }
+                }
+
+        
+           
+            
+        
+        }
+        }
+    }
